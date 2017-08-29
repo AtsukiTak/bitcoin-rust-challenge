a17c3f002ff8048cf31498015f6e13267d8ff05a
AtsukiTak <takatomgoo@gmail.com>
2017-08-29 23:04:48 +0900

    Alter COMMAND_NAME

--- bitcoinr-codec/src/message/command/addr.rs
+++ bitcoinr-codec/src/message/command/addr.rs
@@ -4,7 +4,7 @@ use error::*;
 
 
 /// It means "addr"
-pub const COMMAND_NAME: [u8; 12] = [0x61, 0x64, 0x64, 0x72, 0, 0, 0, 0, 0, 0, 0, 0];
+pub const COMMAND_NAME: [u8; 12] = [b'a', b'd', b'd', b'r', 0, 0, 0, 0, 0, 0, 0, 0];
 
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
--- bitcoinr-codec/src/message/command/get_addr.rs
+++ bitcoinr-codec/src/message/command/get_addr.rs
@@ -3,7 +3,7 @@ use bytes::Bytes;
 use error::*;
 
 /// It means "getaddr"
-pub const COMMAND_NAME: [u8; 12] = [0x67, 0x65, 0x74, 0x61, 0x64, 0x64, 0x72, 0, 0, 0, 0, 0];
+pub const COMMAND_NAME: [u8; 12] = [b'g', b'e', b't', b'a', b'd', b'd', b'r', 0, 0, 0, 0, 0];
 
 
 pub fn encode() -> Result<Bytes> {
