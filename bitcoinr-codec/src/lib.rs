f2934f6ee8f7c7415f090e811e3eab3b7562a22e
AtsukiTak <takatomgoo@gmail.com>
2017-08-29 23:28:44 +0900

    Add crate level comment

--- bitcoinr-codec/src/lib.rs
+++ bitcoinr-codec/src/lib.rs
@@ -1,3 +1,12 @@
+//! `bitcoinr-codec` crate enables you to decode/encode bitcoin p2p network messge into/from
+//! Rust style structures.
+//! Decodec message is `Message` struct which consists `network_type` and `command` field.
+//! Command is main enums which represents each message command.
+//! Some command contains payload such as VersionPayload.
+//!
+//! Each command payload contains almost all field defined in bitcoin protocol.
+//! It means, you can specify any fields of each message even `user_agent` or `addr_trans_port`.
+
 extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
